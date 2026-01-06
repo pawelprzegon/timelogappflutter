dd00b7aee5858e44eea046bfd84ff985e4e5dee8
pawelprzegon <p.przegon@gmail.com>
2026-01-06 11:23:10 +0100

    weather fix

--- lib/features/weather/ui/weather_widget.dart
+++ lib/features/weather/ui/weather_widget.dart
@@ -131,7 +131,6 @@ class _Content extends StatelessWidget {
           ),
           const SizedBox(width: 18),
           Expanded(
-            flex: 2,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
